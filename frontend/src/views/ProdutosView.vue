36db98dc363692d3b70e15fd5ddd8d1087de1a2c
willianxzy <willianlittig14@gmail.com>
2024-08-08 21:12:27 -0300

    test: component para exibir o produto

--- frontend/src/views/ProdutosView.vue
+++ frontend/src/views/ProdutosView.vue
@@ -1,4 +1,3 @@
-
 <script setup>
 import ListarProduto from '../components/ListarProduto.vue'
 </script>
@@ -11,14 +10,5 @@ import ListarProduto from '../components/ListarProduto.vue'
 
 <style scoped>
   main{
-    background-color: #23232e;
-    border-color: rgb(3, 19, 32);
-    border-radius: 10px;
-    border-style: inset;
-    border-width: 5px;
-    padding: 7px;
-    padding-bottom: 15px;
-    display: block;
-    width: 92%;
   }
 </style>
